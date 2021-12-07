1a1a083c091ba061840b2f54969ee85374977628
sequainoo <wazatoyung@gmail.com>
2021-12-07 01:54:02 +0000

    Add 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import stderr
+
+def safe_print_integer_err(value):
+    try:
+        print('{:d}'.format(value))
+    except ValueError as e:
+        stderr.write('Exception: {}\n'.format(e))
+        return False
+    except TypeError as e:
+        stderr.write('Exception: {}\n'.format(e))
+        return False
+    else:
+        return True
