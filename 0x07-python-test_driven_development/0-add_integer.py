df2e8cc5e3e2188ec495b396b22bc0abb6dddfa9
sequainoo <wazatoyung@gmail.com>
2021-12-15 00:08:45 +0000

    Add 0-add_integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module provides one funtion `add_integer`.
+"""
+
+
+def add_integer(a, b=98):
+    """Adds 2 numbers.
+
+    Casts every float to an int
+
+    Args:
+        a (int):
+        b (int):
+    
+    Returns:
+        int: The sum of a and b
+    """
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+    if type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+    a = int(a)
+    b = int(b)
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,8 @@
+## Python - Test Driven Development
+> Each file in this repository holds code that illustrates an essential concept of programming,
+> specific to the Python programming language:
+> doctests, unit tests
+
+### Description of what each file shows:
+* main_files ----- folder holds main programs that showcase examples of how to use functions
+* tests ---------- folder holds all tests and edge case checking
