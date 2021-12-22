afd2b57568de2f4ed1f43e8440197a50fb17a2b8
sequainoo <wazatoyung@gmail.com>
2021-12-22 14:12:47 +0000

    Add

--- 0x0A-python-inheritance/0x0B-python-input_output/0-read_file.py
+++ 0x0A-python-inheritance/0x0B-python-input_output/0-read_file.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""This module presents the function read_file.
+"""
+Module 0-read_file
+
+Contains function that reads and prints contents from file
 """
 
 
 def read_file(filename=""):
-    """Reads the given file `filename`.
-
-    Prints to stdout.
-
-    Args:
-        filename (str): The name of file
-        text (str): The text to write to file
-    """
-    with open(filename, 'r', encoding='utf-8') as f:
-        for line in f:
-            print(line, end='')
+    """Read and print text from file"""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read(), end="")
