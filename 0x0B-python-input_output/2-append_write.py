32ba37d719d5f8e7917f90bd9e9bc0f2a88471e5
sequainoo <wazatoyung@gmail.com>
2021-12-22 14:28:21 +0000

    Update 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -10,5 +10,4 @@ def append_write(filename='', text=''):
         text (str): The text to append.
     '''
     with open(filename, 'a', encoding='utf-8') as f:
-        if text and type(text) is str:
-            return f.write(text)
+        return f.write(text)
