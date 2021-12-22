e998597fa991f090dd3169c4d9f4508495b95cae
sequainoo <wazatoyung@gmail.com>
2021-12-22 15:24:48 +0000

    Update 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,10 +9,13 @@ import sys
 def main():
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file =  __import__('6-load_from_json_file').load_from_json_file
+    filename = 'add_item.json'
 
     l = [sys.argv[i] for i in range(1, len(sys.argv))]
-    filename = 'add_item.json'
-    existing_content = load_from_json_file(filename)
+    try:
+        existing_content = load_from_json_file(filename)
+    except:
+        existing_content = []
     content = existing_content + l
     save_to_json_file(content, filename)
 
