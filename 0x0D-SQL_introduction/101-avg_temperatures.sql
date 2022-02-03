25b12f858d82205f44978ebadedbc6c2a8b62115
sequainoo <wazatoyung@gmail.com>
2022-02-03 18:53:38 +0000

    Update 101-avg_temperature.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,8 @@
 -- wget https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/272/temperatures.sql
 -- Load into db
 -- Displays the avg temperature (Fahrenheit) by city ordered by temperature (descending)
+USE hbtn_0c_0;
 SELECT `city` , AVG(`value`) AS avg_temp
 FROM temperature
 GROUP BY `city`
-ORDER BY avg_temp;
+ORDER BY avg_temp DESC;
