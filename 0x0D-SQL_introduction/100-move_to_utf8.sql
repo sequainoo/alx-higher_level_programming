ce5134ed61966c4c12b0c84909912c486755e9d4
sequainoo <wazatoyung@gmail.com>
2022-02-03 16:22:48 +0000

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,5 @@
 -- Converts database hbtn_0c_0 to UTF8
 -- thus utf8mb4, collate utf8mb4_unicode_ci
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
-
-ALTER TABLE first_table
-CHARACTER SET utf8;
-
-ALTER TABLE first_table
-MODIFY name CHARACTER SET utf8;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
