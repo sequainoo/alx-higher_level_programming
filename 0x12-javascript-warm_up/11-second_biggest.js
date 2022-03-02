a74b89a45d840b225e0f387316cdae96e7543180
sequainoo <wazatoyung@gmail.com>
2022-03-02 21:01:16 +0000

    Fix 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,22 +6,22 @@ const arr = [];
 
 if (isNaN(biggest) || isNaN(biggest2)) {
   console.log(0);
-  return;
-}
-if (biggest < biggest2) {
-  let tmp = biggest;
-  biggest = biggest2;
-  biggest2 = tmp;
-}
-for (let i = 4; i < process.argv.length; i++) {
-  arr.push(parseInt(process.argv[i]));
-}
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] >= biggest) {
-    biggest2 = biggest;
-    biggest = arr[i];
-  } else if (arr[i] > biggest2) {
-    biggest2 = arr[i];
+} else {
+  if (biggest < biggest2) {
+    let tmp = biggest;
+    biggest = biggest2;
+    biggest2 = tmp;
   }
+  for (let i = 4; i < process.argv.length; i++) {
+    arr.push(parseInt(process.argv[i]));
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= biggest) {
+      biggest2 = biggest;
+      biggest = arr[i];
+    } else if (arr[i] > biggest2) {
+      biggest2 = arr[i];
+    }
+  }
+  console.log(biggest2);
 }
-console.log(biggest2);
