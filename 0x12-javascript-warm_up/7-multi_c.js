ce34caacf7d78ee83528a26397e1de8e5c43fe7a
sequainoo <wazatoyung@gmail.com>
2022-03-02 19:18:39 +0000

    Add 7-multi_cjs

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let arg = process.argv[2];
+arg = parseInt(arg);
+
+if (isNaN(arg)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < arg; i++) {
+    console.log('C is fun');
+  }
+}
