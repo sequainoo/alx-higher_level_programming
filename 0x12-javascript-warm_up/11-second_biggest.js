91889c3f6c6305da95c90fd7f7eaa0ae987bf41a
sequainoo <wazatoyung@gmail.com>
2022-03-02 20:58:09 +0000

    Fix 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,31 +1,27 @@
 #!/usr/bin/node
 
-function secondBiggest () {
-  let biggest = parseInt(process.argv[2]);
-  let biggest2 = parseInt(process.argv[3]);
-  const arr = [];
+let biggest = parseInt(process.argv[2]);
+let biggest2 = parseInt(process.argv[3]);
+const arr = [];
 
-  if (isNaN(biggest) || isNaN(biggest2)) {
-    console.log(0);
-    return;
-  }
-  if (biggest < biggest2) {
-    const tmp = biggest;
-    biggest = biggest2;
-    biggest2 = tmp;
-  }
-  for (let i = 4; i < process.argv.length; i++) {
-    arr.push(parseInt(process.argv[i]));
-  }
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= biggest) {
-      biggest2 = biggest;
-      biggest = arr[i];
-    } else if (arr[i] > biggest2) {
-      biggest2 = arr[i];
-    }
-  }
-  console.log(biggest2);
+if (isNaN(biggest) || isNaN(biggest2)) {
+  console.log(0);
+  return;
 }
-
-second_biggest();
+if (biggest < biggest2) {
+  let tmp = biggest;
+  biggest = biggest2;
+  biggest2 = tmp;
+}
+for (let i = 4; i < process.argv.length; i++) {
+  arr.push(parseInt(process.argv[i]));
+}
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] >= biggest) {
+    biggest2 = biggest;
+    biggest = arr[i];
+  } else if (arr[i] > biggest2) {
+    biggest2 = arr[i];
+  }
+}
+console.log(biggest2);
