10938f7b88dfad1500fedded31072721850673b7
sequainoo <wazatoyung@gmail.com>
2022-03-02 19:45:42 +0000

    Fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,10 +4,10 @@ function factorial (num) {
   if (isNaN(num)) {
     return 1;
   }
-  if (num == 1 || num == 0) {
+  if (num === 1 || num === 0) {
     return 1;
   }
-  return num * factorial(num-1);
+  return num * factorial(num - 1);
 }
 
 const num = parseInt(process.argv[2]);
