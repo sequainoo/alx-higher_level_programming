e0d49e173583fad3152bb427733af2ef9089a56d
sequainoo <wazatoyung@gmail.com>
2022-03-02 19:34:11 +0000

    Fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-let a = parseInt(process.argv[2]);
-let b = parseInt(process.argv[3]);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 
 function add (a, b) {
   return (a + b);
