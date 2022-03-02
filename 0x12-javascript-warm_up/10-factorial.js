adb83837b829989b089d21b543efa414360802c8
sequainoo <wazatoyung@gmail.com>
2022-03-02 19:44:16 +0000

    Add 10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function factorial (num) {
+  if (isNaN(num)) {
+    return 1;
+  }
+  if (num == 1 || num == 0) {
+    return 1;
+  }
+  return num * factorial(num-1);
+}
+
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
