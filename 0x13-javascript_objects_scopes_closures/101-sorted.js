c1339b957aed89e0e0b2d7e34d8353797614ad7d
sequainoo <wazatoyung@gmail.com>
2022-03-03 01:48:00 +0000

    fix style

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,8 +2,8 @@
 
 const dict = require('./101-data').dict;
 
-let newDic = {};
-for (let key in dict) {
+const newDic = {};
+for (const key in dict) {
   if (newDic[dict[key]] === undefined) {
     newDic[dict[key]] = [];
   }
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -4,7 +4,7 @@ exports.esrever = function (list) {
   let beg = 0;
   let end = list.length - 1;
   while (beg < end) {
-    let tmp = list[beg];
+    const tmp = list[beg];
     list[beg] = list[end];
     list[end] = tmp;
     beg++;
