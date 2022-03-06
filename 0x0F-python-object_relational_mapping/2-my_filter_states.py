f5ee0ccc591826fc5ceb05cc79615e634150db06
sequainoo <wazatoyung@gmail.com>
2022-03-06 12:28:39 -0800

    Add 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''Script that lists all states with name starting N.'''
+
+from unicodedata import name
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    mysql_username = sys.argv[1]
+    mysql_passwd = sys.argv[2]
+    mysql_db = sys.argv[3]
+    state_name = sys.argv[4]
+
+    conn = MySQLdb.connect(
+        host='localhost',
+        user=mysql_username,
+        passwd=mysql_passwd,
+        port=3306,
+        db=mysql_db)
+    cursor = conn.cursor()
+    num_rows = cursor.execute(
+        'SELECT * FROM states\
+        WHERE states.name = "{}"\
+            ORDER BY states.id'.format(state_name))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
