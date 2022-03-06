53138850a25a4d8179b165ec34940efbaf156af7
sequainoo <wazatoyung@gmail.com>
2022-03-06 11:09:31 -0800

    Make script not executable when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 '''Script that lists all states from hbtn_0e_0_usa.'''
+
 import MySQLdb
 import sys
 
-mysql_username = sys.argv[1]
-mysql_passwd = sys.argv[2]
-mysql_db = sys.argv[3]
-
-conn = MySQLdb.connect(
-    host='localhost',
-    user=mysql_username,
-    passwd=mysql_passwd,
-    db=mysql_db)
-cursor = conn.cursor()
 
 if __name__ == '__main__':
+    mysql_username = sys.argv[1]
+    mysql_passwd = sys.argv[2]
+    mysql_db = sys.argv[3]
+
+    conn = MySQLdb.connect(
+        host='localhost',
+        user=mysql_username,
+        passwd=mysql_passwd,
+        port=3306,
+        db=mysql_db)
+    cursor = conn.cursor()
     num_rows = cursor.execute('SELECT * FROM states ORDER BY states.id')
     rows = cursor.fetchall()
     for row in rows:
