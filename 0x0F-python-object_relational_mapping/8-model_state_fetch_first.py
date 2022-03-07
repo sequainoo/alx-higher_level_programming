f49022620194d8fd8667b68de844dc90e8391757
sequainoo <wazatoyung@gmail.com>
2022-03-06 20:41:11 -0800

    Add 8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Prints the first State Object fro database'''
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == '__main__':
+    conn_str = 'mysql+mysqldb://{:s}:{:s}@localhost:3306/{:s}'\
+        .format(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(conn_str,  pool_pre_ping=True)
+    session = sessionmaker(bind=engine)()
+    first_state = session.query(State).order_by(State.id).first()
+    print('{:d}: {:s}'.format(first_state.id, first_state.name))
+    session.close()
