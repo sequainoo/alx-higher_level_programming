f337c7cf4106233493f5628aefee1ba1d2b1eadd
sequainoo <wazatoyung@gmail.com>
2022-03-07 09:28:53 -0800

    Add 101

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''Script creates a State with an associated city using relationship'''
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, joinedload
+from relationship_state import State
+from relationship_city import Base, City
+
+if __name__ == "__main__":
+    conn_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3],
+    )
+    engine = create_engine(conn_str, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    for state in session.query(State).\
+            options(joinedload(State.cities)).\
+            order_by(State.id).all():
+        print('{:d}: {:s}'.format(state.id, state.name))
+        for city in state.cities:
+            print('\t{:d}: {:s}'.format(city.id, city.name))
+    session.close()
