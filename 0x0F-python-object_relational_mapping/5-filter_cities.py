27cb7f323c16dd08577270a7eee333ccf73d2ea4
sequainoo <wazatoyung@gmail.com>
2022-03-06 16:04:32 -0800

    Add 5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''Script queries cities of a given state.
+
+Pass state to it as argument and would retrieve all
+Associated cities.
+'''
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(user=argv[1],
+                           passwd=argv[2],
+                           host='localhost',
+                           port=3306,
+                           db=argv[3])
+    cursor = conn.cursor()
+    sql = 'SELECT cities.name\
+           FROM cities JOIN states\
+           ON cities.state_id = states.id\
+           WHERE states.name LIKE %s\
+           ORDER BY cities.id'
+    cursor.execute(sql, (argv[4],))
+    rows = cursor.fetchall()
+    length = len(rows)
+    i = 0
+    for row in rows:
+        if i + 1 == length:
+            print(row[0])
+        else:
+            print(row[0], end=', ')
+        i += 1
