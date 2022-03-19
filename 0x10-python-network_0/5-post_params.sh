2b803cff9a503ce4db7cdbb753ebebd8b4a5dafb
sequainoo <wazatoyung@gmail.com>
2022-03-19 04:55:34 -0700

    Edit 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a post request to server and returns response body
-curl --data-urlencode "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s --data "email=test@gmail.com&subject=I will always be here for PLD" "$1"
