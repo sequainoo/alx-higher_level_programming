1417271efdaf9aaba5c429a36b63978fdefdeb76
sequainoo <wazatoyung@gmail.com>
2022-03-21 16:03:15 -0700

    Add 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''fetches a url using requests.'''
+import requests
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    res = requests.get(url)
+    print('Body response:')
+    print('\t- type: {}'.format(type(res.text)))
+    print('\t- content: {}'.format(res.text))
