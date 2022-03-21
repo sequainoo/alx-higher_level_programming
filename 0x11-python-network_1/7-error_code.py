d714a1d2549728f66f0b05601d277bc1d7162eed
sequainoo <wazatoyung@gmail.com>
2022-03-21 16:44:39 -0700

    Add 7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Sends a request with requests.
+
+if error code is equal to or graeter than 400 it handles it.
+'''
+import sys
+import requests
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    res = requests.get(url)
+    if res.status_code >= 400:
+        print('Error code: {}'.format(res.status_code))
+    else:
+        print(res.text)
+
+    
