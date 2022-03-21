452b2d9ea82d55bd793ee99d19aaada5da2ccf48
sequainoo <wazatoyung@gmail.com>
2022-03-21 03:39:18 -0700

    Edit 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,16 +2,13 @@
 '''Sends http request to the url and returns value of X-Request-Id header.'''
 import sys
 from urllib import request
-from urllib.error import HTTPError, URLError
+
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    print(url)
     try:
         with request.urlopen(url) as response:
             x_request_id = response.headers.get('X-Request-Id')
             print(x_request_id)
-    except HTTPError as e:
-        pass
-    except URLError as e:
+    except BaseException as e:
         pass
