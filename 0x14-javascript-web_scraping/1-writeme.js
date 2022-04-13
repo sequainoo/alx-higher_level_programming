46e1d7475d55149edc77f84f2d7654b5985c3a2a
sequainoo <wazatoyung@gmail.com>
2022-04-13 02:14:26 -0700

    Fix: add semi colons: 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-const filename = process.argv[2]
-const data = process.argv[3]
+const filename = process.argv[2];
+const data = process.argv[3];
 
 fs.writeFile(filename, data, (err) => {
   if (err) {
